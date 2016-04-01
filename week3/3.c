b5645a95f4df48a2bb7f6a389d1c507e23435540
Galaxy-yy <x-art@outlook.com>
2016-04-01 22:15:26 +0800

    comment forget.

--- week3/3.c
+++ week3/3.c
@@ -3,12 +3,12 @@
 
 int main() {
     int N,i,n;
-    printf("Input N:\n");
+//  printf("Input N:\n");
     scanf("%d", &N);
     unsigned short a,b,c;
     n = sizeof(unsigned short) * CHAR_BIT;
     for(i=0;i<N;i++){
-        printf("Input a:\n");
+//      printf("Input a:\n");
         scanf("%hu", &a);
         b = a >> (n/2);
         c = a & ((1 <<(n/2)) - 1);
